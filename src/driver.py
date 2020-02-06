da93bdcdf55ca83c28804fd1da53056eb9481242
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-02-05 23:31:56 -0800

    proper setup

--- src/driver.py
+++ src/driver.py
@@ -1,44 +1,17 @@
 #!/usr/bin/env python3
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-import itertools
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from mlxtend.classifier import EnsembleVoteClassifier
-from mlxtend.data import iris_data
-from mlxtend.plotting import plot_decision_regions
+import csv
+from ftfutils import log, Mode
 
-# Initializing Classifiers
-clf1 = LogisticRegression(random_state=0)
-clf2 = RandomForestClassifier(random_state=0)
-clf3 = SVC(random_state=0, probability=True)
-eclf = EnsembleVoteClassifier(clfs=[clf1, clf2, clf3],
-                              weights=[2, 1, 1], voting='soft')
+def main():
+    training_file = open("data/mnist_demo.csv")
+    raw_csv = csv.reader(training_file, delimiter=',')
+    labels = []
+    training_data = []
 
-# Loading some example data
-X, y = iris_data()
-X = X[:,[0, 2]]
+    for i, row in enumerate(raw_csv):
+        labels.append(row[:1])
+        training_data.append(row[1:])
 
-# Plotting Decision Regions
+    log(Mode.DEBUG, str(labels[0]) + "<" + str(len(labels[0])) + ">" + ": " + str(training_data[0]) + "<" + str(len(training_data[0])) + ">")
 
-gs = gridspec.GridSpec(2, 2)
-fig = plt.figure(figsize=(10, 8))
-
-labels = ['Logistic Regression',
-          'Random Forest',
-          'RBF kernel SVM',
-          'Ensemble']
-
-for clf, lab, grd in zip([clf1, clf2, clf3, eclf],
-                         labels,
-                         itertools.product([0, 1],
-                         repeat=2)):
-    clf.fit(X, y)
-    ax = plt.subplot(gs[grd[0], grd[1]])
-    fig = plot_decision_regions(X=X, y=y,
-                                clf=clf, legend=2)
-    plt.title(lab)
-
-plt.show()
+if __name__ == "__main__": main()
