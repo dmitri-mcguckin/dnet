7ec477f32ea9f7337fa42a43bfe929c938fcddf1
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-02-07 18:42:54 -0800

    added some stuf

--- src/driver.py
+++ src/driver.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-import csv
+import csv, numpy
 from neural_network import NeuralNetwork
 from ftfutils import log, Mode
+
+def preprocess(input):
+    t = []
+    print(input)
+    for i in input:
+        print(i)
+        t.append(float(i[0] / 255))
+    return t
 
 def main():
     # Runtime stuff
@@ -16,8 +24,9 @@ def main():
         training_data.append(row[1:])
     file.close()
 
-    dnet = NeuralNetwork(labels, training_data, 3, 0.9)
+    # dnet = NeuralNetwork(preprocess(labels), preprocess(training_data), 1, len(training_data), 0.9)
 
-    log(Mode.DEBUG, "Loaded NN: " + str(dnet))
+    log(Mode.INFO, str(preprocess([255, 120, 69])))
+    # log(Mode.DEBUG, "Loaded NN: " + str(dnet))
 
 if __name__ == "__main__": main()
--- src/neural_network.py
+++ src/neural_network.py
@@ -1,26 +1,27 @@
 import numpy, random
 
 class NeuralNetwork:
-    def __init__(self, labels, input, hidden_layers, momentum=0.75):
-        self.input = numpy.shape(input)[1]
-        self.labels = numpy.shape(labels)[1]
+    def __init__(self, labels, input, hidden_layers, beta=1, momentum=0.75):
+        self.labels = labels
+        self.input = input
 
-        self.data = numpy.shape(input)[0]
+        # self.input = numpy.shape(input)[1]
+        # self.labels = numpy.shape(labels)[1]
+
         self.hidden_layers = hidden_layers
+        self.beta = len(input)
         self.momentum = momentum
 
-        self.weights = []
+        self.weights = numpy.random.rand(len(input), beta)
 
-        self.weights_1 = (numpy.random.rand(self.input + 1, self.hidden_layers) - 0.5) * 2 / numpy.sqrt(self.input)
-        self.weights_2 = (numpy.random.rand(self.hidden_layers + 1, self.labels) - 0.5) * 2 / numpy.sqrt(self.hidden_layers)
+        # self.weights_1 = (numpy.random.rand(self.input + 1, self.hidden_layers) - 0.5) * 2 / numpy.sqrt(self.input)
+        # self.weights_2 = (numpy.random.rand(self.hidden_layers + 1, self.labels) - 0.5) * 2 / numpy.sqrt(self.hidden_layers)
 
     def forward(self, input): pass
 
     def __str__(self):
         return "in=" + str(self.input) \
                 + ", out=" + str(self.labels) \
-                + ", data=" + str(self.data) \
                 + ", hlc=" + str(self.hidden_layers) \
                 + ", mntm=" + str(self.momentum) \
-                + ", l1w=" + str(self.weights_1) \
-                + ", l2w=" + str(self.weights_2)
+                + ", weights=" + str(self.weights)
