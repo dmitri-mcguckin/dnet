8681be70b6c0ec3e0cda0761039ed68a63af8f62
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-02-08 22:33:48 -0800

    added forward feed

--- src/driver.py
+++ src/driver.py
@@ -1,32 +1,67 @@
 #!/usr/bin/env python3
-import csv, numpy
+import csv, numpy, os.path, sys
 from neural_network import NeuralNetwork
 from ftfutils import log, Mode
 
+# Read in the CSV file, then separate the data between targets and data
+#   - It is assumed that the first collumn of each row is the target in the
+#       training data
+def retrieve_from_csv(filename):
+    # Check if file exists
+    if(not os.path.isfile(filename)): raise FileNotFoundError("File does not exist: " + filename)
+
+    labels = []
+    data = []
+
+    # Read from the file and turn it into a CSV object
+    file = open(filename)
+    raw_csv_data = csv.reader(file, delimiter=',')
+
+    # Separate the data accordingly then close the buffer
+    for row in raw_csv_data:
+        labels.append(float(row[:1][0]))
+
+        s_row = []
+        for i in row[1:]:
+            s_row.append(float(i))
+        data.append(s_row)
+
+    file.close()
+
+    return labels, data
+
 def preprocess(input):
     t = []
-    print(input)
-    for i in input:
-        print(i)
-        t.append(float(i[0] / 255))
+    for i in input: t.append(float(i / 255))
     return t
 
 def main():
-    # Runtime stuff
-    labels = []
-    training_data = []
+    try: labels, data = retrieve_from_csv("./mnist_data/dummy_demo.csv")
+    except FileNotFoundError as e:
+        log(Mode.ERROR, str(e))
+        sys.exit(-1)
 
-    # Read the data file
-    file = open("data/mnist_demo.csv")
-    raw_csv = csv.reader(file, delimiter=',')
-    for i, row in enumerate(raw_csv):
-        labels.append(row[:1])
-        training_data.append(row[1:])
-    file.close()
+    # Preprocess the data
+    labels = preprocess(labels)
+    _pd = []
+    for row in data:
+        _pd.append(preprocess(row))
+    data = _pd
 
-    # dnet = NeuralNetwork(preprocess(labels), preprocess(training_data), 1, len(training_data), 0.9)
+    # Validate the data
+    if(len(labels) != len(data)):
+        log(Mode.Error, "Mismatched label-data sizes: (labels " + str(len(labels)) + ", data: " + str(len(data)) + ")")
+        sys.exit(-1)
 
-    log(Mode.INFO, str(preprocess([255, 120, 69])))
-    # log(Mode.DEBUG, "Loaded NN: " + str(dnet))
+    log(Mode.INFO, "label-data: (" + str(len(labels)) + ", " + str(len(data)) + ") | nodes-per-row: " + str(len(data[0])))
+
+    training_model = NeuralNetwork(labels, data, 1)
+
+    for i, row in enumerate(data):
+        output = training_model.feed_forward(row, 0)
+        log(Mode.INFO, "Output: " + str(output)
+                        + "\n\tTarget: " + str(labels[i]))
+
+
 
 if __name__ == "__main__": main()
--- src/neural_network.py
+++ src/neural_network.py
@@ -1,27 +1,53 @@
-import numpy, random
+import numpy
+
+from ftfutils import log, Mode # TODO: remove this when done
+
+DEBUG = False
 
 class NeuralNetwork:
-    def __init__(self, labels, input, hidden_layers, beta=1, momentum=0.75):
-        self.labels = labels
-        self.input = input
+    def __init__(self, labels, data, output, layers=1, width=None, beta=1, momentum=0.75, bias=1, weight=None):
+        self.labels = labels # The labels (aka targets)
+        self.data = data # The input data
+        self.output = output # The number of output nodes
+        self.layers = layers # The number of layers in the network
+        if(width is None): self.width = len(self.data) # The width of each layer
+        else: self.width = width #   - Defaults to width of input
+        self.beta = beta # Hyperparameter for... something...
+        self.momentum = momentum # Hyper parameter for gradient descent
+        self.bias = bias # Hyper parameter for bias
+        if(weight is None): self.weights = numpy.random.rand(self.layers + 1, self.width) - 0.5
+        else: self.weights = numpy.tile(weight, (self.layers + 1, self.width))
 
-        # self.input = numpy.shape(input)[1]
-        # self.labels = numpy.shape(labels)[1]
+    # Sigmoid activation function
+    def activate(self, input): return numpy.around(1 / (1 + numpy.exp(-1 * input)), 3)
 
-        self.hidden_layers = hidden_layers
-        self.beta = len(input)
-        self.momentum = momentum
+    # Softmax for output layer
+    def softmax(self, input): return input
 
-        self.weights = numpy.random.rand(len(input), beta)
+    def feed_forward(self, input, layer=0):
+        if(DEBUG): log(Mode.DEBUG, "W: " + str(self.weights))
+        # If no args, kick off the feed forward
+        # if(input is None): return self.feed_forward(self.data[0], 0)
+        # If we've recured down all layers, process output layer
+        if(layer == self.layers):
+            if(DEBUG): log(Mode.DEBUG, "Reached layer #" + str(layer) + " which is the last hidden layer, with data: " + str(input))
+            output = []
+            for node in range(0, self.output):
+                h = self.weights[layer][0] * self.bias
+                for data in range(0, self.width - 1):
+                    h += self.weights[layer][data + 1] * self.data[layer][data]
+                output.append(self.softmax(self.activate(h)))
+            return output
 
-        # self.weights_1 = (numpy.random.rand(self.input + 1, self.hidden_layers) - 0.5) * 2 / numpy.sqrt(self.input)
-        # self.weights_2 = (numpy.random.rand(self.hidden_layers + 1, self.labels) - 0.5) * 2 / numpy.sqrt(self.hidden_layers)
+        if(DEBUG): log(Mode.DEBUG,  "N: " + str(self.width) + " | W: " + str(self.weights[layer]) + " | D: " + str(input))
 
-    def forward(self, input): pass
+        activations = []
+        for node in range(0, self.width):
+            # Start with the product of the bias and weight
+            h = self.weights[layer][0] * self.bias
+            # Sum the product of weights and inputs
+            for data in range(0, self.width - 1):
+                h += self.weights[layer][data + 1] * self.data[layer][data]
+            activations.append(self.activate(h))
 
-    def __str__(self):
-        return "in=" + str(self.input) \
-                + ", out=" + str(self.labels) \
-                + ", hlc=" + str(self.hidden_layers) \
-                + ", mntm=" + str(self.momentum) \
-                + ", weights=" + str(self.weights)
+        return self.feed_forward(activations, layer + 1)
