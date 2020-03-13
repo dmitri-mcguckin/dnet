cd88f871c429080698bf55a69060682c3bee33cd
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-03-12 22:03:33 -0700

    finished fuzzy c means

--- fuzzy_c_means/src/driver.py
+++ fuzzy_c_means/src/driver.py
@@ -19,6 +19,7 @@ class Point:
         self.y = y
         self.k = k
         self.i = cen_i
+        self.r = -1
         self.k_grades = []
 
         # If the point is a centroid, initialize the grades as a mask
@@ -51,6 +52,9 @@ def plot(ax, k, data, size=2, marker=None, color_override=None):
             if(color_override is not None): color = color_override
             else: color = COLOR_MAP[i]
             ax.scatter(x, y,c=color, s=size, marker=marker)
+        if(data[i].i is not None):
+            c = plt.Circle((data[i].x, data[i].y), data[i].r, color=COLOR_MAP[i], alpha=0.1)
+            ax.add_artist(c)
 
 def init_colors(k):
     res = []
@@ -76,44 +80,39 @@ def init_data(filename, k):
     file.close()
     return res
 
-def classify_data(centroids, data):
+def classify_data(m, centroids, data):
     did_change = False
-
+    exp = (2 / (m - 1))
     for d in data:
-        # Get all the distances for the denominator
-        denom = 0
-        for c in centroids: denom += d.distance(c)
-        old_grades = d.k_grades
-
-        # Check the rest
-        for i, c in enumerate(centroids): d.k_grades[i] = d.distance(c) / denom
-
-        # Determine if the grades converged
-        did_change = (old_grades == d.k_grades)
+        distances = []
+        old_grades = d.k_grades.copy()
+        for c in centroids: distances.append(d.distance(c))
+        for i, c in enumerate(centroids):
+            if(sum(distances) == 0 or distances[i] == 0): continue
+            d.k_grades[i] = (1 / (distances[i] / sum(distances))**exp)
+        if(d.k_grades != old_grades): did_change = True
+        c = centroids[d.k_class()]
+        if(d.distance(c) > c.r): c.r = d.distance(c)
     return did_change
 
 def update_centroids(centroids, data):
+    res = []
     for i, c in enumerate(centroids):
         numer = Point(0, 0, len(centroids), i)
-        grades = []
+        denom = 0
 
         for d in data:
             w = d.k_grades[i]
-            grades.append(w)
             numer = numer + (d * w)
-        centroids[i] = numer / sum(grades)
-        print(str(numer) + "/" + str(sum(grades)))
+            denom += w
+        res.append(numer / denom)
+    return res
 
 def main(args):
     global COLOR_MAP
-    if(len(args) < 2):
-        print("usage: run <filename> <k-clusters> <optional: time-per-epoch (seconds)> <optional: --no-hang>")
+    if(len(args) < 3):
+        print("usage: run <filename> <k-clusters> <fuzzifier> <optional: time-per-epoch (seconds)> <optional: --no-hang> <max iterations>")
         sys.exit(-1)
-
-    # Plot initialization
-    fig, ax = plt.subplots(1, 1, constrained_layout=True)
-    fig.suptitle("K = " + str(args[1]), fontsize=12)
-    ax.set_facecolor('black')
 
     # Variables
     prgm_id = int(time.time())
@@ -123,29 +122,35 @@ def main(args):
     data = init_data(args[0], k)
     COLOR_MAP = init_colors(k)
     centroids = init_centroids(k)
-    m = 2
-    if(len(args) >= 3): playback_time = float(args[2])
+    m = int(args[2])
+    if(len(args) >= 4): playback_time = float(args[3])
     else: playback_time =  1.5
-    if(len(args) >= 4 and args[3] == "--no-hang"): no_hang = True
+    if(len(args) >= 5 and args[4] == "--no-hang"): no_hang = True
     else: no_hang = False
+    if(len(args) >= 6): max_iters = int(args[5])
+    else: max_iters = None
 
-    while(did_change):
+    # Plot initialization
+    fig, ax = plt.subplots(1, 1, constrained_layout=True)
+    fig.suptitle("K = " + str(args[1]) + ", M = " + str(args[2]), fontsize=12)
+    ax.set_facecolor('black')
+
+    while((max_iters is None or itter_count < max_iters) and did_change):
         ax.set_title("Iteration #" + str(itter_count + 1))
-        did_change = classify_data(centroids, data) # Run the classification
+        did_change = classify_data(m, centroids, data) # Run the classification
 
         plot(ax, k, data)
         plot(ax, k, centroids, marker="+", size=80, color_override="white")
 
-        print("\n")
         plt.pause(playback_time)
 
-        update_centroids(centroids, data) # Update the centroids
+        centroids = update_centroids(centroids, data) # Update the centroids
         if(itter_count == 0): plt.gcf().savefig('imgs/' + str(prgm_id) + '_begin.png', dpi=100)
         elif(not did_change): plt.gcf().savefig('imgs/' + str(prgm_id) + '_end.png', dpi=100)
         itter_count += 1
         ax.cla()
 
-    print("Completed k-means in", itter_count, "iterations!")
+    print("Completed fuzzy-c-means in", itter_count, "iterations!")
     if(not no_hang): # Show the window until the user exits
         print("Hanging untill user exit...")
         plt.show()
